f097b4d148c9040bd2cd64aa06aeb44261e0d5b6
Jonathan Hensley <developer.jhensley@gmail.com>
2019-05-09 11:15:10 -0700

    Remove hard-codedpaths to iOS app and Android apk

--- HelloWorldApp.UITest/AppInitializer.cs
+++ HelloWorldApp.UITest/AppInitializer.cs
@@ -10,12 +10,22 @@ namespace HelloWorldApp.UITest
         {
             if (platform == Platform.Android)
             {
+                // Cross-platform path builder
+                // This path to the apk file is unnecessary if the Android project is referenced by UITest project
+                // @"..\..\..\HelloWorldApp\HelloWorldApp.Android\bin\debug\com.companyname.HelloWorldApp.apk"
+                var apkPath = new string[] { "..", "..", "..", "HelloWorldApp", "HelloWorldApp.Android", "bin", "Debug", "com.companyname.HelloWorldApp.apk" };
                 return ConfigureApp.Android
-                    .ApkFile(@"..\..\..\HelloWorldApp\HelloWorldApp.Android\bin\debug\com.companyname.HelloWorldApp.apk")
-                    .StartApp();
+                //.ApkFile(System.IO.Path.Combine(apkPath))
+                .StartApp();
             }
 
-            return ConfigureApp.iOS.StartApp();
+            // This simulator device id and path to the iOS app file is unnecessary if the iOS project is referenced by UITest project
+            //const string simId = "B6FCC117-58C5-4E98-AB6C-6AF581FEE300";// iPhone 6s 12.2 Simulator
+            //const string iPhoneSimulatorAppPath = @"../../../HelloWorldApp/HelloWorldApp.iOS/bin/iPhoneSimulator/debug/device-builds/iphone8.1-12.2/HelloWorldApp.iOS.app";
+            return ConfigureApp.iOS
+                //.AppBundle(iPhoneSimulatorAppPath)
+                //.DeviceIdentifier(simId)
+                .StartApp();
         }
     }
 }
